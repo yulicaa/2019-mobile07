ea354d7f28e24b67d32d68fba93741338225dbb0
Putra Prima Arhandi <putraprima@gmail.com>
2019-10-05 06:35:00 +0700

    starter code

---
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/MainActivity.java
@@ -0,0 +1,16 @@
+package polinema.ac.id.androiduistarter.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import polinema.ac.id.androiduistarter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
