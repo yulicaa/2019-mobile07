b672c4cb86dbb077801310fd05ce88b8e0b9c474
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2019-10-10 00:17:56 +0700

    Membuat navigation dan beberapa fragmen

---
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/fragments/DiscountFragment.java
@@ -0,0 +1,32 @@
+package polinema.ac.id.androiduistarter.fragments.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import polinema.ac.id.androiduistarter.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DiscountFragment extends Fragment {
+
+
+    public DiscountFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_discount, container, false);
+    }
+
+}
