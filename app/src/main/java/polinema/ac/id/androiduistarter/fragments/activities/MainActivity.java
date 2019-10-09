23c9eb08bd70d3e3bf1198a16327dd8758ffff45
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2019-10-10 00:26:45 +0700

    Memperbaharui code pada fragmen

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/MainActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/MainActivity.java
@@ -1,16 +1,61 @@
 package polinema.ac.id.androiduistarter.fragments.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import polinema.ac.id.androiduistarter.R;
+import polinema.ac.id.androiduistarter.fragments.fragments.DiscountFragment;
+import polinema.ac.id.androiduistarter.fragments.fragments.FoodFragment;
+import polinema.ac.id.androiduistarter.fragments.fragments.MovieFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new FoodFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_food:
+                fragment = new FoodFragment();
+                break;
+            case R.id.action_movie:
+                fragment = new MovieFragment();
+                break;
+            case R.id.action_discount:
+                fragment = new DiscountFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
